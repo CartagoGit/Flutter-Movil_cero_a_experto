fedb38f1a6c3200c4c24b2b883ba834d659214ac
Cartago <cartago.relaxingcup@gmail.com>
2023-09-09 00:59:28 +0200

    Probamos los tipos dynamic

--- 01-dart-introduccion/02_tipos_variables.dart
+++ 01-dart-introduccion/02_tipos_variables.dart
@@ -5,11 +5,20 @@ void main() {
   final List<String> abilities = ['impostor'];
   final sprites = <String>['pikachu/fron.png', 'pikachu/back.png'];
 
+  // dynamic comienza como null
+  dynamic errorMessage = 'Un error';
+  // dynamic? successMessage = 'El tipo dynamic no puede ser nulleable porque ya puede ser nulo de por si';
+  errorMessage = null;
+  errorMessage = true;
+  // errorMessage++; Generaria un error porque no se puede sumar un entero a un booleano
+  // etc...
+
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
 }
