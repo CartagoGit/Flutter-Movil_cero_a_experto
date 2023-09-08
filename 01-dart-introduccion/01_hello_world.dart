3e3859e1a9c4791344b39b8ee34932f02cc81795
Cartago <cartago.relaxingcup@gmail.com>
2023-09-09 00:41:15 +0200

    Creamso el primer hello world con dart

---
+++ 01-dart-introduccion/01_hello_world.dart
@@ -0,0 +1,10 @@
+void main() {
+  print('Hello World');
+  // var myName = 'Mario';
+  // final myName = 'Mario';
+  // late final myName = 'Mario';
+  String myName = 'Mario';
+  print('Hello $myName!');
+  print('Hello ${myName.toUpperCase()}!');
+  print('Hello ${2 + 3}!');
+}
