a69cae5734ecadb282f4e0e3ce04e7f0dd0f7ccf
Cartago <cartago.relaxingcup@gmail.com>
2023-09-09 19:15:39 +0200

    Terminamos la clase de los maps de dart

---
+++ 01-dart-introduccion/03_maps.dart
@@ -0,0 +1,21 @@
+
+void main() {
+  final numeros = {
+    1,
+    2,
+    3,
+  };
+
+  final Map<String, dynamic> pokemon = {
+    'nombre': 'Charmander',
+    'tipo': 'Fuego',
+    'poder': 10,
+    'isAlive': true,
+    'abilities': ['Bola de fuego', 'Lanzallamas'],
+    'sprites': {'front': 'charmander/front.png', 'back': 'charmander/back.png'},
+  };
+
+  print({pokemon, numeros, pokemon['nombre']});
+  print(pokemon['sprites']['back']);
+  print(pokemon['sprites'].runtimeType);
+}
