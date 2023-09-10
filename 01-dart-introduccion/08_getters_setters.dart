7b8e6f0d9088407c9f1f3bb73aae6f258e3e862f
Cartago <cartago.relaxingcup@gmail.com>
2023-09-10 15:23:47 +0200

    Creamos una clase abstracta

--- 01-dart-introduccion/08_getters_setters.dart
+++ 01-dart-introduccion/08_getters_setters.dart
@@ -1,5 +1,5 @@
 void main() {
-  final cuadrado = new Square(side: -5);
+  final cuadrado = new Square(side: 5);
   // cuadrado.side = 12;
   print(cuadrado);
   print('Area: ${cuadrado.calculateArea()}');
@@ -18,7 +18,7 @@ class Square {
   double get area => this._side * this._side;
 
   set side(double value) {
-    if (value <= 0) throw ('El lado no puede ser menor o igual a 0');
+    if (value <= 0) throw 'El lado no puede ser menor o igual a 0';
     this._side = value;
   }
 
