4c002f4df35870e749d4b0653d965499a571231a
Cartago <cartago.relaxingcup@gmail.com>
2023-09-10 14:18:58 +0200

    Hacemos pruebas con override de clases y añadimos lint para mostrar error si se sobreescribe un metodo sin override

--- 01-dart-introduccion/06_clases.dart
+++ 01-dart-introduccion/06_clases.dart
@@ -1,22 +1,31 @@
 void main() {
   final wolverine = new Hero(power: 'Regeneración', name: 'Logan');
+  final chachito = new Hero(name: 'Chachote');
 
   print(wolverine);
+  print(chachito);
 }
 
 class Hero {
   String name;
   String power;
 
-  Hero({required name, required power})
-      : name = name,
-        power = power;
+  // Hero({required name, required power})
+  //     : name = name,
+  //       power = power;
+  Hero({required this.name, this.power = 'No tiene poderes'});
 
   String _pepe() {
+    // privado
     return 'pepe';
   }
 
-  String retornaNombre() {
+  String retornaPepe() {
     return this._pepe();
+  }
+
+  @override
+  String toString() {
+    return 'Heroe: ${this.name} - ${this.power}';
   }
 }
