5c6b49b5922e723219fddb4864fe4db080959fd5
Cartago <cartago.relaxingcup@gmail.com>
2023-09-10 15:18:11 +0200

    Terminamos la calse de aserciones

---
+++ 01-dart-introduccion/08_getters_setters.dart
@@ -0,0 +1,26 @@
+void main() {
+  final cuadrado = new Square(side: -5);
+  // cuadrado.side = 12;
+  print(cuadrado);
+  print('Area: ${cuadrado.calculateArea()}');
+  print('Area: ${cuadrado.area}');
+  print('Lado: ${cuadrado._side}');
+}
+
+class Square {
+  late double _side;
+
+  Square({required double side}) : assert(side > 0) {
+    if (side <= 0) throw 'El lado no puede ser menor o igual a 0';
+    _side = side;
+  }
+
+  double get area => this._side * this._side;
+
+  set side(double value) {
+    if (value <= 0) throw ('El lado no puede ser menor o igual a 0');
+    this._side = value;
+  }
+
+  double calculateArea() => this._side * this._side;
+}
